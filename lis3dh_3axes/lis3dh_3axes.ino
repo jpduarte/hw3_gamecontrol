6b615aaadb48c35efc19822ee4a4d9dbf419d871
jpduarte <jpduarte@berkeley.edu>
2016-11-05 01:53:43 -0700

    mat is added for redbear duo code, it scan mat and pint the values, an analog input is used to read each time, 3 analog demux are used, 1 for rows, and 2 for columns

--- lis3dh_3axes/lis3dh_3axes.ino
+++ lis3dh_3axes/lis3dh_3axes.ino
@@ -17,6 +17,26 @@ SYSTEM_MODE(MANUAL);
 #define PI 3.1415
 #define button D8
 
+#define vcc 3.3;
+
+//values from mat matrix
+float mat_values[96];
+
+//analog input
+int analog_read = A0;
+
+//rows control
+int row0 = D2;
+int row1 = D3;
+int row2 = D4;
+//byte rwo[] = {D2,D3,D4};
+
+//column control
+int column0 = D9;
+int column1 = D10;
+int column2 = D11;
+int column3 = D12;
+
 typedef struct ACCEL_DATA {
   float xG, yG, zG;
   float pitch, roll;
@@ -31,16 +51,181 @@ void setup(void) {
   
   Serial.begin(9600);
   pinMode(button,INPUT_PULLUP);
+  pinMode(row0, OUTPUT);
+  pinMode(row1, OUTPUT);  
+  pinMode(row2, OUTPUT);  
+
+  pinMode(column0, OUTPUT);  
+  pinMode(column1, OUTPUT);  
+  pinMode(column2, OUTPUT);  
+  pinMode(column3, OUTPUT);  
+
+  pinMode(analog_read, INPUT);   
   
   LIS3DH_init();
 }
 
 void loop() {
   LIS3DH_getData();
+  get_mat_values();
   LIS3DH_sendData();
+  MAT_sendData();
+  Serial.printf("\n");
   delay(33); //30Hz update rate
 }
+void MAT_sendData(void) {
+  for (int index = 0;index<96;index++) {
+     Serial.printf("%f,",mat_values[index]);  
+  }  
+}
 
+float read_mat(void){
+  int analog_value;
+  float analog_value_float;
+  
+  analog_value = analogRead(analog_read);
+  analog_value_float = analog_value * vcc ; 
+  analog_value_float = analog_value_float /  4095.0;
+  return analog_value_float;
+}
+
+void get_mat_values(void){
+
+  int index=0;
+  //loop row first
+  for (int rowcount = 0;rowcount<3;rowcount++) {
+     set_row(rowcount);
+     //then loop columns
+     for (int columncount = 0;columncount<12;columncount++) {
+      set_column(columncount);
+      //save value at each point of mat
+      mat_values[index]=read_mat();
+      index=index+1;
+     }
+  }
+  
+}
+
+void set_column(int column) {
+  switch (column) {
+    case 0:
+      digitalWrite(column0,LOW);
+      digitalWrite(column1,LOW);
+      digitalWrite(column2,LOW);
+      break;
+    case 1:
+      digitalWrite(column0,HIGH);
+      digitalWrite(column1,LOW);
+      digitalWrite(column2,LOW);
+      digitalWrite(column2,LOW);
+      break;
+    case 2:
+      digitalWrite(column0,LOW);
+      digitalWrite(column1,HIGH);
+      digitalWrite(column2,LOW);
+      digitalWrite(column2,LOW);
+      break;
+    case 3:
+      digitalWrite(column0,HIGH);
+      digitalWrite(column1,HIGH);
+      digitalWrite(column2,LOW);
+      digitalWrite(column2,LOW);
+      break;
+    case 4:
+      digitalWrite(column0,LOW);
+      digitalWrite(column1,LOW);
+      digitalWrite(column2,HIGH);
+      digitalWrite(column2,LOW);
+      break;
+    case 5:
+      digitalWrite(column0,HIGH);
+      digitalWrite(column1,LOW);
+      digitalWrite(column2,HIGH);
+      digitalWrite(column2,LOW);
+      break;
+    case 6:
+      digitalWrite(column0,LOW);
+      digitalWrite(column1,HIGH);
+      digitalWrite(column2,HIGH);
+      digitalWrite(column2,LOW);
+      break;
+    case 7:
+      digitalWrite(column0,HIGH);
+      digitalWrite(column1,HIGH);
+      digitalWrite(column2,HIGH);
+      digitalWrite(column2,LOW);
+      break;      
+    case 8:
+      digitalWrite(column0,LOW);
+      digitalWrite(column1,LOW);
+      digitalWrite(column2,LOW);
+      digitalWrite(column2,HIGH);
+      break;
+    case 9:
+      digitalWrite(column0,HIGH);
+      digitalWrite(column1,LOW);
+      digitalWrite(column2,LOW);
+      digitalWrite(column2,HIGH);
+      break;
+    case 10:
+      digitalWrite(column0,LOW);
+      digitalWrite(column1,HIGH);
+      digitalWrite(column2,LOW);
+      digitalWrite(column2,HIGH);
+      break;
+    case 11:
+      digitalWrite(column0,HIGH);
+      digitalWrite(column1,HIGH);
+      digitalWrite(column2,LOW);
+      digitalWrite(column2,HIGH);
+      break;       
+  }
+}
+
+void set_row(int row) {
+  switch (row) {
+    case 0:
+      digitalWrite(row0,LOW);
+      digitalWrite(row1,LOW);
+      digitalWrite(row2,LOW);
+      break;
+    case 1:
+      digitalWrite(row0,HIGH);
+      digitalWrite(row1,LOW);
+      digitalWrite(row2,LOW);
+      break;
+    case 2:
+      digitalWrite(row0,LOW);
+      digitalWrite(row1,HIGH);
+      digitalWrite(row2,LOW);
+      break;
+    case 3:
+      digitalWrite(row0,HIGH);
+      digitalWrite(row1,HIGH);
+      digitalWrite(row2,LOW);
+      break;
+    case 4:
+      digitalWrite(row0,LOW);
+      digitalWrite(row1,LOW);
+      digitalWrite(row2,HIGH);
+      break;
+    case 5:
+      digitalWrite(row0,HIGH);
+      digitalWrite(row1,LOW);
+      digitalWrite(row2,HIGH);
+      break;
+    case 6:
+      digitalWrite(row0,LOW);
+      digitalWrite(row1,HIGH);
+      digitalWrite(row2,HIGH);
+      break;
+    case 7:
+      digitalWrite(row0,HIGH);
+      digitalWrite(row1,HIGH);
+      digitalWrite(row2,HIGH);
+      break;      
+  }
+}
 boolean LIS3DH_init(void) {
         if(!lis.begin(0x18)) {
                 Serial.printf("Couldn't start!\n");
@@ -55,7 +240,7 @@ boolean LIS3DH_init(void) {
 }
 
 void LIS3DH_sendData(void) {
-        Serial.printf("%f,%f,%i\n",accel_data.pitch,accel_data.roll,!digitalRead(button));
+        Serial.printf("%f,%f,%i,",accel_data.pitch,accel_data.roll,!digitalRead(button));
 }
 
 void LIS3DH_getData(void) {
@@ -76,3 +261,5 @@ void LIS3DH_getData(void) {
         accel_data.pitch = accel_data.pitch * (180/PI);
         accel_data.roll = accel_data.roll * (180/PI);
 }
+
+
